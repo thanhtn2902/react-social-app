f10839278b9b7d3d888b3c3eaca31b28b02ed503
Thanh Truong-TPQ384 <TPQ384@motorolasolutions.com>
2022-08-09 17:30:18 +0700

    update home screen

--- App.jsx
+++ App.jsx
@@ -1,23 +1,111 @@
 import React from 'react';
-import { NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Ionicons from 'react-native-vector-icons/Ionicons';
 import HomeScreen from './screens/HomeScreen';
 import TweetScreen from './screens/TweetScreen';
 import NewTweet from './screens/NewTweet';
 import ProfileScreen from './screens/ProfileScreen';
+import SettingsScreen from './screens/SettingsScreen';
+import SearchScreen from './screens/SearchScreen';
+import Notifications from './screens/Notifications';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
+
+const HomeStackNavigator = () => {
+    return (
+        <Stack.Navigator screenOptions={{
+                headerShown: true,
+                headerBackTitleVisible: false
+            }}
+        >
+            <Stack.Screen
+                name="Home1"
+                component={TabNavigator}
+                options={{
+                    headerShown: false
+                }}
+            />
+            <Stack.Screen
+                name="New Tweet"
+                component={NewTweet}
+                options={{
+                    title: ''
+                }}
+            />
+            <Stack.Screen
+                name="Tweet Screen"
+                component={TweetScreen}
+                options={{
+                    title: ''
+                }}
+            />
+            <Stack.Screen
+                name="Profile Screen"
+                component={ProfileScreen}
+                options={{
+                    title: ''
+                }}
+            />
+        </Stack.Navigator>
+    );
+}
+
+const TabNavigator = () => {
+    return (
+        <Tab.Navigator
+            screenOptions={{
+                headerShown: false,
+                tabBarShowLabel: false
+            }}
+        >
+            <Tab.Screen
+                name="Home2"
+                component={HomeScreen}
+                options={{
+                    tabBarIcon: ({color, size}) => (
+                        <Ionicons name="ios-home" size={size} color={color} />
+                    )
+                }}
+            />
+            <Tab.Screen
+                name="Search"
+                component={SearchScreen}
+                options={{
+                    tabBarIcon: ({color, size}) => (
+                        <Ionicons name="ios-search" size={size} color={color} />
+                    )
+                }}
+            />
+            <Tab.Screen
+                name="Notification"
+                component={Notifications}
+                options={{
+                    tabBarIcon: ({color, size}) => (
+                        <Ionicons name="ios-notifications" size={size} color={color} />
+                    )
+                }}
+            />
+        </Tab.Navigator>
+    );
+}
 
 export default function App() {
     return (
         <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Home" component={HomeScreen} />
-                <Stack.Screen name="New Tweet" component={NewTweet} />
-                <Stack.Screen name="Tweet Screen" component={TweetScreen} />
-                <Stack.Screen name="Profile Screen" component={ProfileScreen} />
-            </Stack.Navigator>
+            <Drawer.Navigator
+                initialRouteName='Home'
+                screenOptions={{
+                    headerShown: true
+                }}
+            >
+                <Drawer.Screen name="Home" component={HomeStackNavigator} />
+                <Drawer.Screen name="Settings" component={SettingsScreen} />
+            </Drawer.Navigator>
         </NavigationContainer>
     );
 }
--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,22 +1,135 @@
-import { View, Text, Button } from 'react-native'
+import { View, Text, Button, StyleSheet, FlatList, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 
-export default function HomeScreen({navigation}) {
+export default function HomeScreen({ navigation }) {
+    let DATA = [
+        {
+            id: '1',
+            title: 'Title 1'
+        },
+        {
+            id: '2',
+            title: 'Title 2'
+        },
+        {
+            id: '3',
+            title: 'Title 3'
+        },
+        {
+            id: '4',
+            title: 'Title 4'
+        },
+        {
+            id: '5',
+            title: 'Title 5'
+        },
+        {
+            id: '6',
+            title: 'Title 6'
+        },
+        {
+            id: '7',
+            title: 'Title 7'
+        },
+        {
+            id: '8',
+            title: 'Title 8'
+        },
+        {
+            id: '9',
+            title: 'Title 9'
+        },
+        {
+            id: '10',
+            title: 'Title 10'
+        }
+    ];
+    let randomAvatar = 'https://i.pravatar.cc/300'
+
+    function gotoProfile() {
+        navigation.navigate('Profile Screen');
+    }
+
+    function gotoSingleTweet() {
+        navigation.navigate('Tweet Screen');
+    }
+
+    const renderItem = ({item}) => (
+        <View style={styles.tweetContainer}>
+            <TouchableOpacity onPress={() => gotoProfile()}>
+                <Image
+                    style={styles.avatar}
+                    source={{
+                        uri: randomAvatar
+                    }}
+                />
+            </TouchableOpacity>
+            <View style={{flex: 1}} >
+                <TouchableOpacity style={styles.tweetFlex} onPress={() => gotoProfile()}>
+                    <Text numberOfLines={1} style={styles.tweetName}>
+                        {item.title}
+                    </Text>
+                    <Text numberOfLines={1} style={styles.tweetHandle}>
+                        @nathan99
+                    </Text>
+                    <Text numberOfLines={1} style={styles.tweetHandle}>
+                        &middot;
+                    </Text>
+                    <Text numberOfLines={1} style={styles.tweetHandle}>
+                        9m
+                    </Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.tweetContentContainer} onPress={() => gotoSingleTweet()}>
+                    <Text style={styles.tweetContent}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi voluptates ipsa voluptate optio aut architecto blanditiis facilis soluta sit hic?
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Home Screen / Feed</Text>
-            <Button
-                title="New Tweet"
-                onPress={() => navigation.navigate('New Tweet')} >
-            </Button>
-            <Button
-                title="Tweet Screen"
-                onPress={() => navigation.navigate('Tweet Screen')} >
-            </Button>
-            <Button
-                title="Profile Screen"
-                onPress={() => navigation.navigate('Profile Screen')} >
-            </Button>
+        <View style={styles.container}>
+            <FlatList
+                data={DATA}
+                renderItem={renderItem}
+                keyExtractor={item => item.id}
+            />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white'
+    },
+    tweetFlex: {
+        flexDirection: 'row'
+    },
+    tweetContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        paddingHorizontal: 12,
+        paddingVertical: 12
+    },
+    avatar: {
+        height: 42,
+        width: 42,
+        marginRight: 8,
+        borderRadius: 21
+    },
+    tweetName: {
+        fontWeight: 'bold',
+        color: '#222222'
+    },
+    tweetHandle: {
+        marginHorizontal: 8,
+        color: 'gray'
+    },
+    tweetContentContainer: {
+        marginTop: 4,
+    },
+    tweetContent: {
+        lineHeight: 20
+    }
+})
